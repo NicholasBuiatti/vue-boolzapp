9ae1a382a7ca60e29029eacee0b4aa3e3c8a1364
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2024-05-16 15:11:52 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue
+
+createApp({
+
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+
+    }
+
+}).mount('#app');
+
+// axios.get('indirizzo Api')
+
+//                 .then((result) => {
+//                     variabileSalvata = result.data.response; ======>  oggetto preso e salvato
+//                     
+//                 });
